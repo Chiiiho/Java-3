c1fc71b52ed88f021eea91fb5e500317c9c36739
chiiiho <163135461+Chiiiho@users.noreply.github.com>
2024-03-16 22:09:10 +0900

    Java-3

---
+++ src/MapMain.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain {
+    public static void main(String[] args) {
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put("Croatia", "Zagreb");
+        userMap.put("Hungary", "Budapest");
+        userMap.put("Austria", "Vienna");
+
+        System.out.println(userMap.get("Croatia"));
+        System.out.println(userMap.get("Hungary"));
+        System.out.println(userMap.get("Austria"));
+
+        String city = userMap.get("Czech Republic");
+        if (city != null) {
+            System.out.println(city);
+        } else {
+            System.out.println("I could not find any city.");
+        }
+    }
+}
+
+
